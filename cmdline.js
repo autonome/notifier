c6cce0a70c2a44eb07f7bb8123020e00ca43dc30
Dietrich Ayala <autonome@gmail.com>
2013-12-10 10:51:59 -0800

    initial commit

---
+++ cmdline.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var io = require('socket.io-client')
+  , program = require('commander')
+
+var args = program
+  .version('0.0.1')
+  .option('-m, --msg <text>', 'The text of the message to send')
+  .parse(process.argv);
+
+if (args.msg.length) {
+  var socket = io.connect('http://dietrich-notifier.jit.su')
+  socket.on('connect', function () {
+    socket.emit('cc7511e2-87df-400e-a25f-9f5bcdcf6a44', args.msg)
+    process.exit()
+  })
+}
---
+++ main.js
@@ -0,0 +1,24 @@
+var io = require('socket.io').listen(app)
+  , fs = require('fs')
+  , app = require('http').createServer(handler)
+
+app.listen();
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.on('cc7511e2-87df-400e-a25f-9f5bcdcf6a44', function (input) {
+    io.sockets.emit('msg', input);
+  });
+});
