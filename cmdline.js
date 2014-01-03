8e33b6da57059210b33e5bc7adf055b62fab416b
Dietrich Ayala <autonome@gmail.com>
2014-01-02 23:09:59 -0800

    server change

--- cmdline.js
+++ cmdline.js
@@ -9,7 +9,7 @@ var args = program
   .parse(process.argv);
 
 if (args.msg.length) {
-  var socket = io.connect('http://dietrich-notifier.jit.su')
+  var socket = io.connect('http://metafluff.com:9001')
   socket.on('connect', function () {
     socket.emit('cc7511e2-87df-400e-a25f-9f5bcdcf6a44', args.msg)
     process.exit()
